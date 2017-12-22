3560252dac945b21d8b723ed1635f981aaa3837f
mrzou <zjzfbyxz@163.com>
2017-12-22 10:09:19 +0800

    push

--- README.md
+++ README.md
@@ -3,4 +3,20 @@
 `create-react-app myAppName`<br>
 `npm run eject 弹出webpack自定义配置,注意是不可逆的`<br>
 `cnpm install express --save`<br>
-`cnpm install -g nodemon`
+`cnpm install -g nodemon 安装后台自动重启服务工具`
+
+### mongodb安装
+###### 官网下载后，直接点击安装，安装路径F:\mongodb\
+`F:\mongodb\bin\mongod.exe
+ mongod -dbpath "F:\mongodb\data\db"
+ mongod --config "F:\mongodb\mongo.conf"
+ mongod --config "F:\mongodb\mongo.conf" --install --serviceName "MongoDB"
+ net start MongoDB  `
+
+###### mongo.conf配置文件：
+`dbpath=F:\mongodb\data #数据库路径
+logpath=F:\mongodb\data\db\log\MongoDB.log #日志输出文件路径
+logappend=true #错误日志采用追加模式
+journal=true #启用日志文件，默认启用
+quiet=true #这个选项可以过滤掉一些无用的日志信息，若需要调试使用请设置为false
+port=27017 #端口号 默认为27017`
--- server/server.js
+++ server/server.js
@@ -2,14 +2,63 @@
  * Created by Mrzou on 2017-12-12.
  */
 const express = require('express')
+const mongoose = require('mongoose')
+//链接mongo
+//const DB_URL = 'mongodb://127.0.0.1:27017'
+const DB_URL = 'mongodb://localhost:27017/imooc'
+
+//需先开启mongodb服务器cmd > net start MongoDB
+mongoose.connect(DB_URL)
+
+mongoose.connection.on('connected', function () {
+  console.log('mongo connect success')
+})
+
+//类似于mysql的表 mongo里有文档、字段的概念
+const User = mongoose.model('user', new mongoose.Schema({
+  user: {type: String, require: true},
+  age: {type: Number, require: true}
+}))
+
+//新增数据
+//User.create({
+//  user: 'xiaoming',
+//  age: 21
+//}, function (err, doc) {
+//  if (!err) {
+//    console.log(doc)
+//  } else {
+//    console.log(err)
+//  }
+//})
+
+//删除age:18的所有数据
+//User.remove({age: 18}, (err, doc)=> {
+//  console.log(doc)
+//})
+
+//修改数据
+//User.update({'user': 'mrzou'}, {'$set': {age: 16}}, (err, doc)=> {
+//  console.log(doc)
+//})
+
 //新建App
 const app = express()
 
 app.get('/', function (req, res) { //请求，响应
   res.send('<h1>Hello world999</h1>')
 })
-app.get('/data',function(req,res){
-  res.json({name:'zou',type:'computer'})
+app.get('/data', function (req, res) {
+  //查询mongodb数据库数据
+  //User.find({}, function (err, doc) {
+  //  res.json(doc) //无条件查询所有数据,返回[{xx:aa},{rr:bb}]集合结构数据
+  //})
+  User.find({user:'xiaoming'}, function (err, doc) {
+    res.json(doc) //条件查询user:'xiaoming'的数据,返回[{xx:aa},{rr:bb}]集合结构数据
+  })
+  //User.findOne({user:'xiaoming'}, function (err, doc) {
+  //  res.json(doc) //findOne只查询一条，查到就返回，且返回结构为{rr:bb}单条数据
+  //})
 })
 app.listen(9093, function () {
   console.log('node app start at port 9093')
