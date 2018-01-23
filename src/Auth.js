f76a55c8f33cfde1f6bcfe014410bbd4247d8b8a
mrzou <zjzfbyxz@163.com>
2018-01-23 15:37:20 +0800

    测试node proxy代理服务处理端口跨域问题

--- src/Auth.js
+++ src/Auth.js
@@ -1,26 +1,36 @@
-/**
- * Created by Mrzou on 2017-12-27.
- */
 import React from 'react'
-import {connect} from 'react-redux'
-import {login} from './Auth.redux'
+import { connect } from 'react-redux'
+import { login,getUserData } from './Auth.redux'
 import {Redirect} from 'react-router-dom'
+import axios from 'axios'
 
-//两个reducers, 每一个reducers都有一个state
-//合并reducers
+// 两个reducers 每个reducers都有一个state
+// 合并reducers
 @connect(
-  state=>state.auth, {login}
+  state=>state.auth,
+  {login, getUserData}
 )
-
-class Auth extends React.Component {
-  constructor(props) {
-    super(props)
+class Auth extends React.Component{
+  // constructor(props) {
+  // 	super(props)
+  // 	this.state={
+  // 		data:{}
+  // 	}
+  // }
+  componentDidMount() {
+    this.props.getUserData()
+    // axios.get('/data')
+    // 	.then(res=>{
+    // 		if (res.status===200) {
+    // 			this.setState({data:res.data})
+    // 		}
+    // 	})
   }
-
-  render() {
+  render(){
     return (
       <div>
-        {this.props.isAuth && <Redirect to="/dashboard"/>}
+        <h2>我的名字是{this.props.user},年龄{this.props.age}</h2>
+        { this.props.isAuth? <Redirect to='/dashboard' /> : null}
         <h2>你没有权限，需要登录才能看</h2>
         <button onClick={this.props.login}>登录</button>
       </div>
@@ -29,3 +39,4 @@ class Auth extends React.Component {
 }
 
 export default Auth
+
--- src/Auth.redux.js
+++ src/Auth.redux.js
@@ -1,23 +1,44 @@
-/**
- * Created by Mrzou on 2017-12-27.
- */
+import axios from 'axios'
+
 const LOGIN = 'LOGIN'
 const LOGOUT = 'LOGOUT'
-
-export function auth(state = {isAuth: false, user: '李云龙'}, action) {
-  switch (action.type) {
+const USER_DATA='USER_DATA'
+const initState = {
+  isAuth:false,
+  user:'李云龙',
+  age:20
+}
+export function auth(state=initState,action){
+  console.log(state,action)
+  switch(action.type){
     case LOGIN:
-      return Object.assign({}, state, {isAuth: true})
+      return {...state, isAuth:true}
     case LOGOUT:
-      return {...state, isAuth: false}
+      return {...state, isAuth:false}
+    case USER_DATA:
+      return {...state, user:action.payload.user,age:action.payload.age}
     default:
       return state
   }
 }
-
-export function login() {
-  return {type: LOGIN}
+// action
+export function getUserData(){
+  // dispatch用来通知数据修改
+  return dispatch=>{
+    axios.get('/data')
+      .then(res=>{
+        if (res.status===200) {
+          dispatch(userData(res.data))
+        }
+      })
+  }
 }
-export function logout() {
-  return {type: LOGOUT}
+export function userData(data){
+  return {type:USER_DATA,payload:data}
+}
+export function login(){
+  return {type:LOGIN}
+}
+export function logout(){
+  return {type:LOGOUT}
 }
